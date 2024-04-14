b1ae26af399c9ce3f03eb6d1e886f342e3af3f67
Javier Emanuel Huebra <104619744+javierhuebra@users.noreply.github.com>
2024-04-14 14:40:56 -0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://javierhuebra.github.io/Landing_Kscaritas/sw.js', { scope: 'https://javierhuebra.github.io/Landing_Kscaritas/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Landing_Kscaritas/sw.js', { scope: '/Landing_Kscaritas/' })})}
